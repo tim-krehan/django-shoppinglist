e14b6896f39dfbe7841aeae47f767f03bf37cb2c
Tim Krehan <mail@timkrehan.de>
2021-07-12 00:19:20 +0200

    added comments to api calls

--- views/urls.py
+++ views/urls.py
@@ -6,8 +6,13 @@ from . import listentry
 from . import user
 
 urlpatterns = [
+    # shoppinglist
     path('shoppinglist/<int:pk>/', shoppinglist.get.get),
     path('shoppinglist/<int:pk>/list/', listentry.list.list),
+
+    # shoppinglist entries
     path('listentry/<int:pk>/', listentry.get.get),
+
+    # user
     path('user/<int:pk>/', user.get.get),
 ]
